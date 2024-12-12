0b90932826132696022b1edeb20e487b7e5d0102
SAIKARTHIKEYAN-MRNOBUDDY <saikarthikeyaneswaran@gmail.com>
2024-12-12 12:47:22 +0530

    second  commit

--- src/components/TopContent/TopContent.js
+++ src/components/TopContent/TopContent.js
@@ -12,12 +12,14 @@ function TopContent() {
  collaborating <br/>in team environments to deliver optimized solutions.<br/> Passionate about building scalable,
  interactive, <br/>and modern web applications while continuously <br/>learning and growing in the field of
  software development.</p>
-        <a href='www.google.com'>
+        <a href="/resume.pdf" 
+    download="Resume_Kamal.pdf" >
             <button className='topcontent__resumebutton'>Resume</button>
+            </a>
             <Link to='projects' smooth={true} duration={500}>
             <button className='topcontent__workbutton'>My Work</button>
             </Link>
-        </a>
+        
         </div>
       
     </div>
